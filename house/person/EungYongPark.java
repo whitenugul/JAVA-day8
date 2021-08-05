d5f07e733dd20d41743f225c8f2e04081872f7ab
whitenugul <andrewahn00@gmail.com>
2021-08-05 23:35:06 +0900

    Create EungYongPark.java

---
+++ house/person/EungYongPark.java
@@ -0,0 +1,11 @@
+package jump2java.house.person;
+
+import jump2java.house.*;
+
+public class EungYongPark extends HousePark{
+    public static void main(String[] args) {
+        EungYongPark eyp = new EungYongPark();
+        System.out.println(eyp.lastname);
+        System.out.println(eyp.info);
+    }
+}
