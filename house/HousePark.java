bb9cf916881673c0555b2e9020ae2c5a6a92ee60
whitenugul <andrewahn00@gmail.com>
2021-08-05 23:35:03 +0900

    Create HousePark.java

---
+++ house/HousePark.java
@@ -0,0 +1,13 @@
+package jump2java.house;
+
+public class HousePark {
+    protected String lastname = "park";
+    public String info = "this is public message";
+    public static void main(String[] args) {
+        // HouseKim kim = new HouseKim();
+        // System.out.println(kim.lastname);
+        HousePark pey = new HousePark();
+        HousePark pes = new HousePark();
+    }
+
+}
